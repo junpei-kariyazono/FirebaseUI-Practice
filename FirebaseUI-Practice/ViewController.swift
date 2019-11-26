e02363a2a8f01b3abfaa4e58340951e66cabbf11
Junpei Kariyazono <karikun.2010@gmail.com>
2019-11-26 10:05:34 +0900

    Initial Commit

---
+++ FirebaseUI-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseUI-Practice
+//
+//  Created by 仮屋園　純平 on 2019/11/26.
+//  Copyright © 2019 仮屋園　純平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
